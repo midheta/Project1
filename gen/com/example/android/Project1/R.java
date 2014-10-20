3b2560b956fc49381bbcff240e5e3bc9e3a22f7a
midheta <midheta.ramic1@gmail.com>
2014-10-20 16:28:41 +0200

    changes

--- gen/com/example/android/Project1/R.java
+++ gen/com/example/android/Project1/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.example.android.BluetoothChat;
+package com.example.android.Project1;
 
 public final class R {
     public static final class attr {
